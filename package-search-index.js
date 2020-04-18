a7b2e37448a1b78bb5f4371541a6e62e86379990
Boomaa23 <cmo93003@yahoo.com>
2020-04-17 22:47:59 -0700

    Update for improved console output

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.boomaa.XKCDViewer.display"},{"l":"com.boomaa.XKCDViewer.draw"},{"l":"com.boomaa.XKCDViewer.reporting"},{"l":"com.boomaa.XKCDViewer.threading"},{"l":"com.boomaa.XKCDViewer.utils"}]
